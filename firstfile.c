68f3910d3ad57a2d72ae899e787d3f44f2fd19e5
LusterPearl <salamioluwatosin90@gmail.com>
2023-08-11 09:03:42 -0700

    updates

--- firstfile.c
+++ firstfile.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	const char *message = ("I will not give up\n");
 
-	write(STDOUT_FILENO, message, izeof(message) - 1);
+	write(STDOUT_FILENO, message, sizeof(message) - 1);
 	return (0);
 }
